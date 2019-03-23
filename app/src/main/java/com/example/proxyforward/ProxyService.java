bc9fbf156a4f2fed1c1fe651978a69fd1bfc9196
lollipop <vladislav.krets@gmail.com>
2019-03-23 12:21:30 +0300

    added notification

--- app/src/main/java/com/example/proxyforward/ProxyService.java
+++ app/src/main/java/com/example/proxyforward/ProxyService.java
@@ -1,9 +1,16 @@
 package com.example.proxyforward;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.io.IOException;
@@ -23,6 +30,26 @@ public class ProxyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "onStartCommand");
         port = intent.getIntExtra("port", 4333);
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("com.example.proxyforward",
+                    "ProxyService", NotificationManager.IMPORTANCE_NONE);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert manager != null;
+            manager.createNotificationChannel(notificationChannel);
+        }
+        startForeground(1, new NotificationCompat.Builder(this,
+                "com.example.proxyforward")
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Server is running")
+                .setContentIntent(pendingIntent)
+                .build());
         startProxy();
         return super.onStartCommand(intent, flags, startId);
     }
